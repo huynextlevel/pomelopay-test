0a57e199c0aa4cfa4b6bf6944d69ca932ea02a4c
huynextlevel <huy@nextlevel.sg>
2022-03-25 01:05:38 +0700

    Remove some unnecessary screens

--- src/routers/RootNavigation.js
+++ src/routers/RootNavigation.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from '../screens/SplashScreen';
-import LoginNavigator from './LoginStack';
-import RegisterNavigator from './RegisterStack';
 import DrawerNavigator from './DrawerNavigator';
 
 const Stack = createStackNavigator();
@@ -16,26 +14,6 @@ export default function NavigatorView() {
         name="Splash"
         component={SplashScreen}
         options={{ headerShown: false, gestureEnabled: false }}
-      />
-      <Stack.Screen
-        name="Login"
-        component={LoginNavigator}
-        options={() => ({
-          headerLeft: null,
-          title: null,
-          gestureEnabled: false,
-          headerShown: false,
-        })}
-      />
-      <Stack.Screen
-        name="Register"
-        component={RegisterNavigator}
-        options={() => ({
-          headerLeft: null,
-          title: null,
-          gestureEnabled: false,
-          headerShown: false,
-        })}
       />
       <Stack.Screen
         name="Main"
